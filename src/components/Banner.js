a6434d928c052313682489fc8a423a6686186dc0
zhaoxuetong <zhaoxuetong@chinahomu.com>
2017-02-15 17:46:20 +0800

    use map to instead forEach

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -21,12 +21,12 @@ class Banner extends React.Component {
   constructor(props) {
     super(props);
     this._imgs = [];
-    this.moveX=0;
-    this.animate=null;
+    this.moveX = 0;
+    this.animate = null;
     this.state = {
       carouselWrap: {
         width: '100%'
-        , transform: 'translateX('+this.moveX+'px)'
+        , transform: 'translateX(' + this.moveX + 'px)'
       }
     };
   }
@@ -37,16 +37,18 @@ class Banner extends React.Component {
     let width = this._imgs[0].clientWidth;
     //console.log(width);
     this.moveX++;
-    if(this.moveX>=num){
-      this.moveX=0;
+    if (this.moveX >= num) {
+      this.moveX = 0;
     }
     this.setState({
-      carouselWrap:{
+      carouselWrap: {
         width: num + '00%'
-        ,transform:'translateX(-'+this.moveX*width+'px)'
+        , transform: 'translateX(-' + this.moveX * width + 'px)'
       }
     });
-    this.animate=setTimeout(()=>{this.move.call(this)},2000)
+    this.animate = setTimeout(() => {
+      this.move.call(this)
+    }, 2000)
   }
 
   componentDidMount() {
@@ -58,39 +60,56 @@ class Banner extends React.Component {
       }
     });
 
-    this.animate=setTimeout(()=>{this.move.call(this)},2000)
+    this.animate = setTimeout(() => {
+      this.move.call(this)
+    }, 2000)
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
     //console.log(this._imgs)
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     clearTimeout(this.animate)
   }
 
   render() {
     //console.log(++renderI);
 
-    let Pics = [];
-    images.forEach((value, index) => {
-      //console.log('index...'+index);
-      Pics.push(
-        <div
-          className="carouselBox"
-          key={index}
-        >
-          <img
-            src={value.src}
-            alt={value.alt}
-            ref={(img) => {
-              //console.warn(renderI+":"+value.alt);//打印出来
-              this._imgs[index]=img;
-            }}
-          />
-        </div>
-      )
-    });
+    let Pics = images.map((value, index) =>
+      <div
+        className="carouselBox"
+        key={index}
+      >
+        <img
+          src={value.src}
+          alt={value.alt}
+          ref={(img) => {
+            //console.warn(renderI+":"+value.alt);//打印出来
+            this._imgs[index] = img;
+          }}
+        />
+      </div>
+    );
+    // images.forEach((value, index) => {
+    //   //console.log('index...'+index);
+    //   Pics.push(
+    //     <div
+    //       className="carouselBox"
+    //       key={index}
+    //     >
+    //       <img
+    //         src={value.src}
+    //         alt={value.alt}
+    //         ref={(img) => {
+    //           //console.warn(renderI+":"+value.alt);//打印出来
+    //           this._imgs[index]=img;
+    //         }}
+    //       />
+    //     </div>
+    //   )
+    // });
+
     //console.log(this._imgs);
     return (
       <div className="Banner">
