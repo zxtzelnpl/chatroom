e661023924999efcd31cc1a2160a900a5e86564f
zhaoxuetong <zhaoxuetong@chinahomu.com>
2017-03-10 15:41:30 +0800

    add produce target

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -3,17 +3,54 @@ import $ from 'jquery';
 
 function Message ({message}) {
     return(
-      <div>
-        <div className="name">{message.from.name}</div>
-        <div className="time">{new Date(message.meta.createAt).toLocaleString()}:</div>
+      <li className="message">
+        <div className="name">{message.from.name}:</div>
+        <div className="time">{new Date(message.meta.createAt).toLocaleString()}</div>
         <div className="content">{message.content}</div>
-      </div>
+      </li>
     )
 }
 
 class MessageBox extends React.Component{
+  constructor(props){
+    super(props);
+    this.mouse={
+      state:'off'
+      ,outerH:0
+      ,innerH:0
+      ,canH:0
+      ,moveH:0
+      ,startH:0
+      ,endH:0
+    };
+    this.state={
+      transform:'translateY(0px)'
+    }
+  }
+
+  handleMouseDown(e){
+    this.mouse.state='on';
+    this.mouse.startH=e.clientY;
+  }
+
+  handleMouseMove(e){
+    var transform;
+    if(this.mouse.state==='on'&&this.mouse.canH>0){
+      this.mouse.moveH=e.clientY-this.mouse.startH;
+      console.log(this.mouse.moveH);
+      transform='translateY('+this.mouse.moveH+'px)';
+      this.setState({
+        transform:transform
+      })
+    }
+  }
+
+  handleMouseUp(){
+    this.mouse.state='off';
+  }
 
   componentDidMount(){
+    console.log('mounted');
     let me=this;
     $.ajax({
       type:'GET'
@@ -26,16 +63,33 @@ class MessageBox extends React.Component{
       }
     })
   }
+  componentDidUpdate(){
+    console.log('update');
+    this.mouse.outerH=this.messagesBox.clientHeight;
+    this.mouse.innerH=this.messages.scrollHeight;
+    this.mouse.canH=this.mouse.innerH-this.mouse.outerH>0?this.mouse.innerH-this.mouse.outerH:0;
+  }
   render(){
     let messages=this.props.messages;
 
     let messagesBox=messages.map((message,index)=>(
-      <Message key={message.content+index} message={message} />
+      <Message key={index} message={message} />
     ));
 
     return(
-      <div className="messagesBox">
-        {messagesBox}
+      <div
+        className="messagesBox"
+        onMouseDown={this.handleMouseDown.bind(this)}
+        onMouseMove={this.handleMouseMove.bind(this)}
+        onMouseUp={this.handleMouseUp.bind(this)}
+        ref={(box)=>{this.messagesBox=box}}
+      >
+        <ul
+          style={this.state}
+          ref={(ul)=>{this.messages=ul}}
+        >
+          {messagesBox}
+        </ul>
       </div>
     )
   }
