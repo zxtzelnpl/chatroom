6daf954d8674e12db67fe8d3bf61173e741f9126
zhaoxuetong <zhaoxuetong@chinahomu.com>
2017-04-01 09:31:17 +0800

    change table to 4

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
-var port=process.env.PORT || 3000;
-var dbUrl='mongodb://localhost/junyin';
+var port = process.env.PORT || 3000;
+var dbUrl = 'mongodb://localhost/junyin';
 
 
-
-var path=require('path');
+var path = require('path');
 
 var mongoose = require('mongoose');
-var express = require('express') ;
+var express = require('express');
 var cookieParser = require('cookie-parser');
 
 var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 var bodyParser = require('body-parser');
 
-var routes=require('./config/routes');
+var routes = require('./config/routes');
 
 var app = express();
-var http=require('http').Server(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set('views','./app/views/pages');
-app.set('view engine','pug');
+app.set('views', './app/views/pages');
+app.set('view engine', 'pug');
 
 //链接数据库
 mongoose.connect(dbUrl);
 mongoose.connection.on('connected', function () {
-  console.log('MongoDateBase connection success!')
+    console.log('MongoDateBase connection success!')
 });
 
 app.use(session({
-  secret:'junyin'
-  ,resave:true
-  ,saveUninitialized:true
-  ,store:new mongoStore({
-    url:dbUrl
-    ,collection:'session'
-    ,maxAge:1000*60*10
-  })
+    secret: 'junyin'
+    , resave: true
+    , saveUninitialized: true
+    , store: new mongoStore({
+        url: dbUrl
+        , collection: 'session'
+        , maxAge: 1000 * 60 * 10
+    })
 }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
-  extended:true
+    extended: true
 }));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-routes(app,io);
+routes(app, io);
 
-http.listen(port,function(){
-  console.log('Express server listening on port ' + port);
+http.listen(port, function () {
+    console.log('Express server listening on port ' + port);
 });
--- app/controllers/message.js
+++ app/controllers/message.js
@@ -2,82 +2,82 @@ var Message = require('../models/message');
 var User = require('../models/user');
 
 exports.getmessage = function (req, res) {
-  let page=req.query.page;
-  let nums=30;
-  Message
-    .find({})
-    .sort({_id:-1})
-    .skip(page*nums)
-    .limit(nums)
-    .populate('from', 'name')
-    .exec(function (err, messages) {
-      console.log(messages);
-      if (err) {
-        console.log(err)
-      }
-      res.json(messages);
-    })
+    let page = req.query.page;
+    let nums = 30;
+    Message
+        .find({})
+        .sort({_id: -1})
+        .skip(page * nums)
+        .limit(nums)
+        .populate('from', 'name')
+        .exec(function (err, messages) {
+            console.log(messages);
+            if (err) {
+                console.log(err)
+            }
+            res.json(messages);
+        })
 };
 
 exports.sendmessage = function (req, res) {
-  var _message = req.query;
+    var _message = req.query;
 
 
-  _message.from = req.session.user._id;
+    _message.from = req.session.user._id;
 
-  var message = new Message(_message);
-  message.save(function (err, message) {
-    if (err) {
-      console.log(err)
-    }
-    res.json({
-      state: 'success'
+    var message = new Message(_message);
+    message.save(function (err, message) {
+        if (err) {
+            console.log(err)
+        }
+        res.json({
+            state: 'success'
+        })
     })
-  })
 };
 
 exports.save = function (msg, next) {
 
-  User.findOne({name: msg.name}, function (err, user) {
-    var _message = {
-      from: {}
-    };
-    if (err) {
-      console.log(err)
-    }
-
-    _message.from = user._id;
-    _message.content = msg.content;
-
-    var message = new Message(_message);
-    message.save(function (err, message) {
-      if (err) {
-        console.log(err);
-      }
-      var _id=message._id;
-      Message
-        .findOne({_id:_id})
-        .populate('from', 'name')
-        .exec(function (err, messages) {
-          if (err) {
+    User.findOne({name: msg.name}, function (err, user) {
+        var _message = {
+            from: {}
+        };
+        if (err) {
             console.log(err)
-          }
-          next(messages);
-        })
-    })
-  });
+        }
 
-  // var _message={
-  //   from:{}
-  // };
-  // _message.from.name=msg.name;
-  // _message.content=msg.content;
-  //
-  // var message=new Message(_message);
-  // message.save(function(err,message){
-  //   if(err){
-  //     console.log(err);
-  //   }
-  //   next(message);
-  // })
+        _message.from = user._id;
+        _message.content = msg.content;
+
+        var message = new Message(_message);
+        message.save(function (err, message) {
+            if (err) {
+                console.log(err);
+            }
+            var _id = message._id;
+            Message
+                .findOne({_id: _id})
+                .populate('from', 'name')
+                .exec(function (err, messages) {
+                    if (err) {
+                        console.log(err)
+                    }
+                    next(messages);
+                })
+        })
+    });
+
+    // var _message={
+    //   from:{}
+    // };
+    // _message.from.name=msg.name;
+    // _message.content=msg.content;
+    //
+    // var message=new Message(_message);
+    // message.save(function(err,message){
+    //   if(err){
+    //     console.log(err);
+    //   }
+    //   next(message);
+    // })
 };
