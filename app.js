aa8bfe75083a5fca1fdeb00240a373b3eb7dbac1
zhaoxuetong <zhaoxuetong@chinahomu.com>
2017-04-01 16:13:46 +0800

    sovle the problem that the sio use session

--- app.js
+++ app.js
@@ -1,50 +1,83 @@
-var port = process.env.PORT || 3000;
-var dbUrl = 'mongodb://localhost/junyin';
+/**定义常量**/
+const port = process.env.PORT || 3000;
+const dbUrl = 'mongodb://localhost/junyin';
+const secret='junyin';
 
+/**引如外部**/
+const path = require('path');
+const mongoose = require('mongoose');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const connectMongo=require('connect-mongo');
+const http = require('http');
+const io=require('socket.io');
+const cookie = require('cookie');
 
-var path = require('path');
+/**引如内部路由**/
+const routes = require('./config/routes');
 
-var mongoose = require('mongoose');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-
-var session = require('express-session');
-var mongoStore = require('connect-mongo')(session);
-var bodyParser = require('body-parser');
-
-var routes = require('./config/routes');
-
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.set('views', './app/views/pages');
-app.set('view engine', 'pug');
-
-//链接数据库
+/**1.链接数据库start**/
 mongoose.connect(dbUrl);
 mongoose.connection.on('connected', function () {
     console.log('MongoDateBase connection success!')
 });
+/**1.链接数据库end**/
 
-app.use(session({
-    secret: 'junyin'
+/**2.建立app和sio对象start*/
+const app = express();
+const server = http.Server(app);
+const sio = io(server);
+/**2.建立app和sio对象end*/
+
+/**3.session持久化start*/
+const mongoStore = connectMongo(session);
+const sessionStore = new mongoStore({
+    mongooseConnection: mongoose.connection
+    , collection: 'session'
+    , ttl: 14 * 24 * 60*60
+});
+const sessionMiddleware=session({
+    secret: secret
     , resave: true
     , saveUninitialized: true
-    , store: new mongoStore({
-        url: dbUrl
-        , collection: 'session'
-        , maxAge: 1000 * 60 * 10
-    })
-}));
-app.use(cookieParser());
+    , store: sessionStore
+    , cookie:{
+        maxAge:24*60*60*1000
+    }
+});
+/**3.session持久化end*/
+
+/**4.parseCookie建立并加密start*/
+const parseCookie = cookieParser(secret);
+/**4.parseCookie建立并加密end*/
+
+
+/**5.模版文件start*/
+app.set('views', './app/views/pages');
+app.set('view engine', 'pug');
+/**5.模版文件end*/
+
+
+/**6.中间件start*/
+app.use(parseCookie);
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(sessionMiddleware);
 app.use(express.static(path.join(__dirname, 'public')));
+sio.use(function(socket, next) {
+    sessionMiddleware(socket.request, socket.request.res, next);
+});
+/**6.中间件end*/
 
-routes(app, io);
+/**7.路由文件start*/
+routes(app, sio,sessionStore);
+/**7.路由文件end*/
 
-http.listen(port, function () {
+/**8.服务器端口监听start*/
+server.listen(port, function () {
     console.log('Express server listening on port ' + port);
 });
+/**8..服务器端口监听start*/
--- config/routes.js
+++ config/routes.js
@@ -2,42 +2,50 @@ var Index = require('../app/controllers/index');
 var User = require('../app/controllers/user');
 var Message = require('../app/controllers/message');
 
-module.exports = function(app,io){
-  let userNum=0;//统计在线人数
-  /*pre handle user*/
-  app.use(function(req,res,next){
-    app.locals.user = req.session.user;
-
-    next();
-  });
-
-  //Index
-  app.get('/',Index.index);
-
-  //User
-  app.post('/signup',User.signup);
-  app.post('/signin',User.signin);
-  app.get('/logout',User.logout);
-
-  //Message
-  app.get('/sendmessage',Message.sendmessage);
-  app.get('/getmessage',Message.getmessage);
-
-  //Message
-  io.on('connection',function(socket){
-    userNum++;
-    io.emit('online',userNum);
-    console.log(userNum+'user is in connection');
-
-    socket.on('chat message', function (msg) {
-      Message.save(msg,function(message){
-        io.emit('chat message',message);
-      });
+module.exports = function (app, io,sessionStore) {
+    let userNum = 0;//统计在线人数
+    /*pre handle user*/
+    app.use(function (req, res, next) {
+        app.locals.user = req.session.user;
+        next();
     });
 
-    socket.on('disconnect',function(){
-      userNum--;
-      io.emit('online',userNum);
+    //Index
+    app.get('/', Index.index);
+
+    //User
+    app.post('/signup', User.signup);
+    app.post('/signin', User.signin);
+    app.get('/logout', User.logout);
+
+    //Message
+    app.get('/sendmessage', Message.sendmessage);
+    app.get('/getmessage', Message.getmessage);
+
+
+    //Message
+    io.on('connection', function (socket) {
+        console.log('##socket##');
+        console.log(socket.request.session.user);
+        console.log('##socket##');
+
+        console.log('##sessionStore##');
+        console.log(sessionStore);
+        console.log('##sessionStore##');
+
+        userNum++;
+        io.emit('online', userNum);
+        console.log(userNum + 'user is in connection');
+
+        socket.on('chat message', function (msg) {
+            Message.save(msg, function (message) {
+                io.emit('chat message', message);
+            });
+        });
+
+        socket.on('disconnect', function () {
+            userNum--;
+            io.emit('online', userNum);
+        })
     })
-  })
 };
